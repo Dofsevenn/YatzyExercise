2162022e6e977b0d34ef3f293d7e3913d54e4813
Kjetil <kjetil.dir@gmail.com>
2019-08-28 16:40:50 +0200

    Added all files

---
+++ src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
@@ -0,0 +1,17 @@
+package com.KjetilBjelldokken.yatzy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatzyTest {
+
+    @Test
+    void shouldCalculatTwos (){
+        assertEquals(4, 2+2);
+    }
+
+
+
+
+}
