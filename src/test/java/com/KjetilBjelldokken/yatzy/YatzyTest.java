4677b799e78381b8ba3cb40f5959882e19d2666f
Kjetil <kjetil.dir@gmail.com>
2019-08-30 16:42:39 +0200

    changes to test

--- src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
+++ src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
@@ -11,6 +11,12 @@ public class YatzyTest {
         assertEquals(4, 2+2);
     }
 
+    @Test
+    void shouldCalculateForOnes() {
+        assertEquals(0, score(ONES, new int[]{2, 3, 4, 5, 6}));
+        assertEquals(0, score(ONES, new int[]{2, 1, 4, 1, 1}));
+        assertEquals(0, score(ONES, new int[]{1, 1, 1, 1, 1}));
+    }
 
 
 
