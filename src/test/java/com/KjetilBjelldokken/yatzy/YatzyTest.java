f1587cd6e08b2d7d886ff1158a0c183ec6bbf5d1
Kjetil <kjetil.dir@gmail.com>
2019-08-31 23:37:59 +0200

    Count ones

--- src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
+++ src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
@@ -7,9 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class YatzyTest {
 
     @Test
-    void shouldCalculatTwos (){
-        assertEquals(4, 2+2);
+    void shouldCalculatTwos() {
+        assertEquals(4, 2 + 2);
     }
 
+    @Test
+    void shouldCalculateOnes() {
+        assertEquals(1, score(Category.ONES, new int[]{2, 1, 4, 5, 6}));
+    }
+
+    enum Category {
+        ONES, TWOS, THREES
+    }
+
+    static int score(Category category, int[] rolls) {
+        int count = 0;
+
+        for(int i = 4; i >= 0; i--) {
+            if(rolls[i] == 1) {
+                count += rolls[i];
+            }
+            System.out.println(count);
+        }
+
+        /*
+      for(int value : rolls) {
+          count += value;
+      } */
+      return count;
+    }
 
 }
