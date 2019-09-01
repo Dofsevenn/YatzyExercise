ebd30ab639c3bd6f488f388b06cd70b29342efe1
Kjetil <kjetil.dir@gmail.com>
2019-09-01 20:33:58 +0200

    Twos to fours

--- src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
+++ src/test/java/com/KjetilBjelldokken/yatzy/YatzyTest.java
@@ -25,19 +25,58 @@ public class YatzyTest {
         assertEquals(5, score(Category.TWOS, new int[]{2, 2, 2, 2, 2}));
     }
 
+    @Test
+    void shouldCalculateForThrees() {
+        assertEquals(0, score(Category.THREES, new int[]{1, 2, 4, 5, 6}));
+        assertEquals(3, score(Category.THREES, new int[]{2, 3, 3, 3, 1}));
+        assertEquals(5, score(Category.THREES, new int[]{3, 3, 3, 3, 3}));
+    }
+
+    @Test
+    void shouldCalculateForFours() {
+        assertEquals(0, score(Category.FOURS, new int[]{1, 2, 3, 5, 6}));
+        assertEquals(3, score(Category.FOURS, new int[]{2, 4, 3, 4, 4}));
+        assertEquals(5, score(Category.FOURS, new int[]{4, 4, 4, 4, 4}));
+    }
+
     enum Category {
-        ONES, TWOS, THREES
+        ONES, TWOS, THREES, FOURS
     }
 
     static int score(Category category, int[] rolls) {
         int count = 0;
-
-        for(int i = 4; i >= 0; i--) {
-            if(rolls[i] == 1) {
-                count += rolls[i];
+        if(category == (Category.ONES)) {
+            for (int i = 4; i >= 0; i--) {
+                if (rolls[i] == 1) {
+                    count += rolls[i];
+                }
             }
+            System.out.println(count + " ones");
+
+        } else if(category == (Category.TWOS)) {
+            for (int i = 4; i >= 0; i--) {
+                if (rolls[i] == 2) {
+                    count += rolls[i] / 2;
+                }
+            }
+            System.out.println(count + " twos");
+
+        } else if(category == (Category.THREES)) {
+            for (int i = 4; i >= 0; i--) {
+                if (rolls[i] == 3) {
+                    count += rolls[i] / 3;
+                }
+            }
+            System.out.println(count + " threes");
+
+        } else if(category == (Category.FOURS)) {
+            for (int i = 4; i >= 0; i--) {
+                if (rolls[i] == 4) {
+                    count += rolls[i] / 4;
+                }
+            }
+            System.out.println(count + " fours");
         }
-        System.out.println(count + " ones");
         return count;
     }
 }
